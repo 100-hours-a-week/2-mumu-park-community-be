a098d7c1d041390053bcd5c4a2ad961defd7d918
박성춘 <songchoon10@naver.com>
2025-04-06 13:19:12 +0900

    refactor: 회원 탈퇴시 무의미한 로직 삭제

--- src/main/java/com/kaboot/community/domain/member/service/member/impl/MemberCommandServiceImpl.java
+++ src/main/java/com/kaboot/community/domain/member/service/member/impl/MemberCommandServiceImpl.java
@@ -8,53 +8,49 @@ import com.kaboot.community.domain.member.entity.Member;
 import com.kaboot.community.domain.member.service.member.MemberCommandService;
 import com.kaboot.community.domain.member.service.member.MemberQueryService;
 import com.kaboot.community.domain.member.service.password.PasswordEncoder;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class MemberCommandServiceImpl implements MemberCommandService {
-    private final MemberQueryService memberQueryService;
-    private final PasswordEncoder passwordEncoder;
 
-    @Override
-    public void update(String authUsername, ModifyRequest modifyRequest) {
-        Member validMember = memberQueryService.getMemberByUsername(authUsername);
+  private final MemberQueryService memberQueryService;
+  private final PasswordEncoder passwordEncoder;
 
-        if (!validMember.isMe(authUsername)) {
-            throw new CustomException(CustomResponseStatus.ACCESS_DENIED);
-        }
+  @Override
+  public void update(String authUsername, ModifyRequest modifyRequest) {
+    Member validMember = memberQueryService.getMemberByUsername(authUsername);
 
-        if (memberQueryService.isNicknameDuplicate(modifyRequest.nickname()).isExist()) {
-            throw new CustomException(CustomResponseStatus.NICKNAME_ALREADY_EXIST);
-        }
-
-        validMember.update(modifyRequest);
+    if (!validMember.isMe(authUsername)) {
+      throw new CustomException(CustomResponseStatus.ACCESS_DENIED);
     }
 
-    @Override
-    public void updatePassword(String authUsername, PasswordUpdateRequest passwordUpdateRequest) {
-        Member validMember = memberQueryService.getMemberByUsername(authUsername);
-
-        if (!validMember.isMe(authUsername)) {
-            throw new CustomException(CustomResponseStatus.ACCESS_DENIED);
-        }
-
-        validMember.updatePassword(passwordEncoder.hash(passwordUpdateRequest.newPassword()));
+    if (memberQueryService.isNicknameDuplicate(modifyRequest.nickname()).isExist()) {
+      throw new CustomException(CustomResponseStatus.NICKNAME_ALREADY_EXIST);
     }
 
-    @Override
-    public void withdrawal(String authUsername) {
-        Member validMember = memberQueryService.getMemberByUsername(authUsername);
+    validMember.update(modifyRequest);
+  }
 
-        if (!validMember.isMe(authUsername)) {
-            throw new CustomException(CustomResponseStatus.ACCESS_DENIED);
-        }
+  @Override
+  public void updatePassword(String authUsername, PasswordUpdateRequest passwordUpdateRequest) {
+    Member validMember = memberQueryService.getMemberByUsername(authUsername);
 
-        validMember.withdrawal(LocalDateTime.now());
+    if (!validMember.isMe(authUsername)) {
+      throw new CustomException(CustomResponseStatus.ACCESS_DENIED);
     }
+
+    validMember.updatePassword(passwordEncoder.hash(passwordUpdateRequest.newPassword()));
+  }
+
+  @Override
+  public void withdrawal(String authUsername) {
+    Member validMember = memberQueryService.getMemberByUsername(authUsername);
+
+    validMember.withdrawal(LocalDateTime.now());
+  }
 }
